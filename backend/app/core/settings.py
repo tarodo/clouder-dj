7f88f3e49ed9e20e7d663c46a3cfc176b37a257f
tarodo <rsvolozhanin@gmail.com>
2025-06-29 19:03:54 +0400

    fix: improve expire time for token

--- backend/app/core/settings.py
+++ backend/app/core/settings.py
@@ -22,7 +22,7 @@ class Settings(BaseSettings):
     JWT_ALGO: str = "HS256"
     BASE_URL: str = "http://localhost:8000"
 
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 24 * 60
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
     SPOTIFY_AUTH_URL: str = "https://accounts.spotify.com/authorize"
